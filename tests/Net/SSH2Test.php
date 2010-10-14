a4e3382e013ed80bd723b02f24cef023d3ec5609
Tim Martin <tim@asymptotic.co.uk>
2010-10-14 16:53:20 +0200

    Got rid of the test of connecting to an invalid server, since it doesn't achieve much test-wise and depends on a valid DNS server

--- tests/Net/SSH2Test.php
+++ tests/Net/SSH2Test.php
@@ -8,16 +8,6 @@ require_once 'Net/SocketHandler.php';
 class Net_SSH2Test extends PHPUnit_Framework_TestCase {
 	/**
 	 * @expectedException Exception
-	 *
-	 * This doesn't test much, but it at least checks that we haven't
-	 * made any syntax errors.
-	 */
-	public function testInstantiateInvalidHost() {
-		$ssh = new Net_SSH2('nonexistent.invalid');
-	}
-
-	/**
-	 * @expectedException Exception
 	 * @expectedExceptionMessage Connection closed by server
 	 *
 	 * If we mock the socket so that it immediately and always returns
